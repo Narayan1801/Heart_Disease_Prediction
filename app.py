a8a7a8ff9cd6d023ee365e9adaf437690e485eee
Narayan1801 <72499092+Narayan1801@users.noreply.github.com>
2021-02-20 13:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import os
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 
 app = Flask(__name__)
 model = pickle.load(open('heart_model.pkl','rb'))
