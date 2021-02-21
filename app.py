1b5b4f84cc3ad2afdee7c59d5a5441eaa2816b51
Narayan Chowdhari <72499092+Narayan1801@users.noreply.github.com>
2021-02-21 15:00:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ model = pickle.load(open('heart_model.pkl','rb'))
 def home():
     return render_template('main.html')
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
    
     int_features = [int(x) for x in request.form.values()]
@@ -19,16 +19,7 @@ def predict():
 
     return render_template('main.html', prediction_text='The Chances of Having Heart Disease is {}'.format(prediction))
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    '''
-    For direct API calls trough request
-    '''
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
